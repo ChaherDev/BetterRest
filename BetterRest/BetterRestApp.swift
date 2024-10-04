39291fbdaacc6c00db6d6b34b419bb7fedea15dd
Chaher Machhour <chaher.croissance.public@gmail.com>
2024-10-04 17:20:08 +0200

    Initial Commit

---
+++ BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by Chaher Machhour on 04/10/2024.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
